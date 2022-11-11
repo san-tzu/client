109b88ed3abc95ed1258b3d36a6194758d56e914
Sahan Thenuwara <t.a.s.sanjith@gmail.com>
2022-11-11 06:23:17 +0530

    added delete icon

--- src/components/LogsTable.jsx
+++ src/components/LogsTable.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
 
 export default function LogsTable(props) {
   return (
@@ -12,22 +14,27 @@ export default function LogsTable(props) {
         >
           <thead className="table-light">
             <tr>
+              <th>#</th>
               <th>Start</th>
               <th>Destination</th>
               <th>Meter Reading</th>
               <th>Other</th>
               <th>Remarks</th>
+              <th></th>
             </tr>
           </thead>
           <tbody className="table-group-divider">
             {props.travels.map((t, i) => {
               return (
                 <tr className="" key={i}>
+                  <td>{i+1}</td>
                   <td>{t.start}</td>
                   <td>{t.destination}</td>
                   <td>{t.meter}</td>
                   <td>{t.other}</td>
                   <td>{t.remark}</td>
+                  <td><FontAwesomeIcon icon={faDeleteLeft} /></td>
+                  {/* <td><FontAwesomeIcon icon="fa-solid fa-delete-left" /></td> */}
                 </tr>
               );
             })}
